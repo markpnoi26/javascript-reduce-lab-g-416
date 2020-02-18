bb62e9e100bd24937bcf5c276259ede0378e13b8
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-02-18 03:05:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const monologueLines = [
   'I am the one who knocks!'
 ];
 
-function totalBatteries() {
+const totalBatteries = () => {
   return batteryBatches.reduce((initialState, item) => {
     return initialState += item
   }, 0)
